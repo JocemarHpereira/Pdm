d5a3793b3b84e83dc95bfc405e7504cd83b38730
JocemarHpereira <jocemarpere@gmail.com>
2020-03-09 02:44:53 -0300

    Ajustado tags do logcat

--- app/src/main/java/com/unisc/pdm/MainActivity.java
+++ app/src/main/java/com/unisc/pdm/MainActivity.java
@@ -3,6 +3,7 @@ package com.unisc.pdm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("log", "Evento OnCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("log", "Evento OnStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("log", "Evento OnResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("log", "Evento OnPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("log", "Evento OnStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("Log", "Evento OnRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("log", "Evento OnDestroy");
     }
 }
